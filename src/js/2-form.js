571f169cddedbfcb2e96a2fce432bacf4f2f92da
lesia_onyshchuk <lkane443@gmail.com>
2024-11-16 00:05:10 +0200

    rework form

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -9,6 +9,18 @@ const textArea = document.querySelector('textarea');
 
 const localStorageKey = 'feedback-form-state';
 
+document.addEventListener('DOMContentLoaded', reloadFoo);
+function reloadFoo() {
+  const storageData = localStorage.getItem(localStorageKey);
+  if (storageData) {
+    const pageReboot = JSON.parse(localStorage.getItem(localStorageKey));
+    input.value = pageReboot.email;
+    textArea.value = pageReboot.message;
+    formData.email = pageReboot.email;
+    formData.message = pageReboot.message;
+  }
+}
+
 form.addEventListener('input', inputFoo);
 function inputFoo() {
   formData.email = input.value.trim();
@@ -16,29 +28,18 @@ function inputFoo() {
   localStorage.setItem(localStorageKey, JSON.stringify(formData));
 }
 
-const pageReboot = JSON.parse(localStorage.getItem(localStorageKey) || {});
-
-if (Object.values(pageReboot).length) {
-  input.value = pageReboot.email;
-  textArea.value = pageReboot.message;
-  formData.email = pageReboot.email;
-  formData.message = pageReboot.message;
-}
-
 form.addEventListener('submit', submitFoo);
 function submitFoo(event) {
-  if (
-    event.target.elements.email.value.trim() === '' ||
-    event.target.elements.message.value.trim() === ''
-  ) {
+  event.preventDefault();
+  const emailValue = event.target.elements.email.value.trim();
+  const messageValue = event.target.elements.message.value.trim();
+  if (!emailValue || !messageValue) {
     alert('Fill please all fields');
     return;
-  } else {
-    event.preventDefault();
-    formData.email = event.target.elements.email.value.trim();
-    formData.message = event.target.elements.message.value.trim();
-    localStorage.removeItem(localStorageKey);
-    form.reset();
-    console.log(formData);
   }
+  formData.email = emailValue;
+  formData.message = messageValue;
+  console.log(formData);
+  localStorage.removeItem(localStorageKey);
+  form.reset();
 }
